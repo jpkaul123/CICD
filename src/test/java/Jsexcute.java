783e6231061b4c097744e838b3f9dae142f2c0c6
jpkaul123 <159743503+jpkaul123@users.noreply.github.com>
2024-03-04 16:58:52 +0530

    Update Jsexcute.java

--- src/test/java/Jsexcute.java
+++ src/test/java/Jsexcute.java
@@ -13,28 +13,14 @@ public class Jsexcute {
 		
 		WebDriver driver = new ChromeDriver();
 		
-		driver.get("https://manage.qa2.dotbricks.net/adminp");
+		driver.get("https://www.google.com");
 		
 		
 		driver.manage().window().maximize();
 		Thread.sleep(5000);
-		driver.findElement(By.id("login_username")).sendKeys("jaiprakash@racknap.com");
-		driver.findElement(By.id("login_password")).sendKeys("abcd1234");
-		driver.findElement(By.id("kt_login_signin_submit_form")).click();
-		Thread.sleep(2000);
-		driver.get("https://manage.qa2.dotbricks.net/adminp/orders/orders-list");
-		Thread.sleep(2000);
-		JavascriptExecutor js=(JavascriptExecutor) driver;
-		js.executeScript("window.scrollBy(0,500)");
-		Thread.sleep(2000);
+		System.out.println("jp is best");
 		
-	List<WebElement> values = driver.findElements(By.cssSelector("tr:nth-child(4) > td:nth-child(4)"));
 	
-	for (int i=0;i<=values.size();i++) {
-		
-		System.out.print(Integer.parseInt(values.get(i).getText().split("-")[1].trim()));
-	}
-//		js.executeScript("document.querySelector('.thead-light').scrollTop=500");
 	}
 
 }
